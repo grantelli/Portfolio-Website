f3acfab1441e7cb6c7a6b3d43b91b68837ace8d5
Grant Elliott <grant1275@gmail.com>
2024-10-09 18:48:43 -0500

    Add typing & floating image animation

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".positions", {
+    strings: ["Software Engineer", "CS Major @ Iowa State", "Fullstack Developer", "Creator"],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 1500,
+    loop: true
+});
